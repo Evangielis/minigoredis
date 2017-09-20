94af7d017ae369143c1a3baae0a35eb019d0fa32
Painton, Lee Wayne <lee.painton@ge.com>
2017-09-20 11:34:10 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,28 @@
+use 'godoc cmd/minigoredis' for documentation on the minigoredis command 
+
+PACKAGE DOCUMENTATION
+
+package minigoredis
+    import "minigoredis"
+
+
+TYPES
+
+type Minigoredis struct {
+    Miniredis *miniredis.Miniredis
+}
+    Minigoredis -- struct which wraps a miniredis instance
+
+func Run() (*Minigoredis, error)
+    Run -- starts a miniredis server and serves it wrapped in Gominiredis
+
+func (g *Minigoredis) Close() error
+    Close -- Closes the miniredis server. Never throws an error
+
+func (g *Minigoredis) Get(key string) *redis.StringCmd
+    Get -- Retrieves a value from miniredis wrapped in a go-redis StringCmd
+
+func (g *Minigoredis) Set(key string, val string, _ time.Duration) *redis.StatusCmd
+    Set -- Sets a key, value pair in miniredis
+
+
---
+++ minigoredis.go
@@ -0,0 +1,42 @@
+package minigoredis
+
+import "github.com/alicebob/miniredis"
+import "github.com/go-redis/redis"
+import "time"
+
+// Minigoredis -- struct which wraps a miniredis instance
+type Minigoredis struct {
+	Miniredis *miniredis.Miniredis
+}
+
+// Run -- starts a miniredis server and serves it wrapped in Gominiredis
+func Run() (*Minigoredis, error) {
+	mr, err := miniredis.Run()
+	return &Minigoredis{
+		Miniredis: mr,
+	}, err
+}
+
+// Get -- Retrieves a value from miniredis wrapped in a go-redis StringCmd
+func (g *Minigoredis) Get(key string) *redis.StringCmd {
+	val, err := g.Miniredis.Get(key)
+	if err != nil {
+		panic(err)
+	}
+	return redis.NewStringResult(val, nil)
+}
+
+// Set -- Sets a key, value pair in miniredis
+func (g *Minigoredis) Set(key string, val string, _ time.Duration) *redis.StatusCmd {
+	err := g.Miniredis.Set(key, val)
+	if err != nil {
+		panic(err)
+	}
+	return redis.NewStatusResult("OK", err)
+}
+
+// Close -- Closes the miniredis server.  Never throws an error
+func (g *Minigoredis) Close() error {
+	g.Miniredis.Close()
+	return nil
+}
